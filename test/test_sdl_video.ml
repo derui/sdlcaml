b9466dbb9084d00feb997ba829a6204da8fc8829
derui <derutakayu@gmail.com>
2012-09-21 07:56:51 +0900

    [from now] 2012/09/21 07:56:51
    diff --git a/test/test_sdl_video.ml b/test/test_sdl_video.ml
    index 8918c0e..b6bd791 100644
    --- a/test/test_sdl_video.ml
    +++ b/test/test_sdl_video.ml
    @@ -4,8 +4,8 @@ open OUnit
     let test_set_up _ =
       begin
         Sdl.sdl_init [`VIDEO];
    -    sdl_set_video_mode ~width:640 ~height:480 ~depth:24
    -      ~flags:[SDL_SWSURFACE];
    +    Sdl_video.sdl_set_video_mode ~width:640 ~height:480 ~depth:24
    +      ~flags:[Sdl_video.SDL_SWSURFACE];
       end
    
     let test_tear_down surface =
    @@ -21,7 +21,8 @@ let test_sdl_video_init surface =
    
     let test_sdl_get_pixel_format surface =
       begin
    -    let format = sdl_get_pixelformat surface in
    +    let format = Sdl_video.sdl_get_pixelformat surface in
    +    let open Sdl_video in
         assert_equal 24 format.bits_per_pixel;
         assert_equal 3 format.bytes_per_pixel;
         assert_equal 0xFF0000 format.rmask;
    @@ -31,24 +32,24 @@ let test_sdl_get_pixel_format surface =
    
     let test_sdl_fill_color surface =
       begin
    +    let open Sdl_video in
         let col = {red = 255; green = 128; blue = 0; alpha = 255} in
    -    Sdl_video.sdl_fill_color ~dist:surface ~fill:col;
    +    Sdl_video.sdl_fill_color ~dist:surface ~fill:col ();
         Thread.delay 5.0;
       end
    
     let test_sdl_blit_surface surface =
       begin
    +    let open Sdl_video in
         let newsurface = Sdl_video.sdl_create_surface
           ~flags:[SDL_SWSURFACE;SDL_SRCALPHA] ~width:300 ~height:200
         and col = {red = 255; green = 128; blue = 0; alpha = 255} in
    -    Sdl_video.sdl_fill_color ~dist:newsurface ~fill:col;
    -    Sdl_video.sdl_blit_surface ~src:newsurface ~dist:surface;
    +    Sdl_video.sdl_fill_color ~dist:newsurface ~fill:col ();
    +    assert_equal BLIT_SUCCESS (Sdl_video.sdl_blit_surface ~src:newsurface ~dist:surface ());
         Thread.delay 3.0;
         Sdl_video.sdl_free_surface newsurface;
       end
    
    -let test_sdl_get
    -
     let suite = "SDL video tests" >:::
       [ "video initialize" >:: (bracket test_set_up test_sdl_video_init
                                    test_tear_down);

--- test/test_sdl_video.ml
+++ test/test_sdl_video.ml
@@ -4,8 +4,8 @@ open OUnit
 let test_set_up _ =
   begin
     Sdl.sdl_init [`VIDEO];
-    sdl_set_video_mode ~width:640 ~height:480 ~depth:24
-      ~flags:[SDL_SWSURFACE];
+    Sdl_video.sdl_set_video_mode ~width:640 ~height:480 ~depth:24
+      ~flags:[Sdl_video.SDL_SWSURFACE];
   end
 
 let test_tear_down surface =
@@ -21,7 +21,8 @@ let test_sdl_video_init surface =
 
 let test_sdl_get_pixel_format surface =
   begin
-    let format = sdl_get_pixelformat surface in
+    let format = Sdl_video.sdl_get_pixelformat surface in
+    let open Sdl_video in
     assert_equal 24 format.bits_per_pixel;
     assert_equal 3 format.bytes_per_pixel;
     assert_equal 0xFF0000 format.rmask;
@@ -31,23 +32,23 @@ let test_sdl_get_pixel_format surface =
 
 let test_sdl_fill_color surface =
   begin
+    let open Sdl_video in
     let col = {red = 255; green = 128; blue = 0; alpha = 255} in
-    Sdl_video.sdl_fill_color ~dist:surface ~fill:col;
+    Sdl_video.sdl_fill_color ~dist:surface ~fill:col ();
     Thread.delay 5.0;
   end
 
 let test_sdl_blit_surface surface =
   begin
+    let open Sdl_video in
     let newsurface = Sdl_video.sdl_create_surface
       ~flags:[SDL_SWSURFACE;SDL_SRCALPHA] ~width:300 ~height:200
     and col = {red = 255; green = 128; blue = 0; alpha = 255} in
-    Sdl_video.sdl_fill_color ~dist:newsurface ~fill:col;
-    Sdl_video.sdl_blit_surface ~src:newsurface ~dist:surface;
+    Sdl_video.sdl_fill_color ~dist:newsurface ~fill:col ();
+    assert_equal BLIT_SUCCESS (Sdl_video.sdl_blit_surface ~src:newsurface ~dist:surface ());
     Thread.delay 3.0;
     Sdl_video.sdl_free_surface newsurface;
   end
-
-let test_sdl_get
 
 let suite = "SDL video tests" >:::
   [ "video initialize" >:: (bracket test_set_up test_sdl_video_init
