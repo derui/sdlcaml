9c58131e0436565c4bad04b84e81078f4965bbb1
derui <derutakayu@gmail.com>
2012-12-28 23:14:57 +0900

    Fixed include files misplace when condition whether enable or disable is disable.

--- src/sdl_image_stubs.c
+++ src/sdl_image_stubs.c
@@ -1,4 +1,3 @@
-#include <SDL_image.h>
 #include <caml/mlvalues.h>
 #include <caml/memory.h>
 #include <caml/alloc.h>
@@ -7,9 +6,10 @@
 #include <caml/fail.h>
 
 #include "common.h"
-#include "sdl_image_flags.h"
 
 #ifdef SDLCAML_IMAGE_ENABLE
+#include <SDL_image.h>
+#include "sdl_image_flags.h"
 #include "sdl_image_stubs.enable.inc"
 #else
 #include "sdl_image_stubs.disable.inc"
--- src/sdl_mixer_stub.c
+++ src/sdl_mixer_stub.c
@@ -1,4 +1,3 @@
-#include <SDL_mixer.h>
 #include <caml/mlvalues.h>
 #include <caml/memory.h>
 #include <caml/alloc.h>
@@ -7,9 +6,10 @@
 #include <caml/fail.h>
 
 #include "common.h"
-#include "sdl_mixer_flags.h"
 
 #ifdef SDLCAML_MIXER_ENABLE
+#include <SDL_mixer.h>
+#include "sdl_mixer_flags.h"
 #include "sdl_mixer_stub.enable.inc"
 #else
 #include "sdl_mixer_stub.disable.inc"
