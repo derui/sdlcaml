5fa34a2fa501f35d8bb3c1fae5912c6f2f5d6824
derui <derutakayu@gmail.com>
2016-03-11 22:45:21 +0900

    Change file names

--- gen/gl_bindings/enum.ml
+++ gen/gl_bindings/enum.ml
@@ -7,91 +7,33 @@
 *)
 open Core.Std
 
-let xml_file = ref ""
-let major_version = ref 0
-let minor_version = ref 0
-
-type group = string * (string list)
-
-module Enum_map = Map.Make(String)
-module Enum = struct
-  type t = int64
-
-  let merge map enums =
-    List.fold_left ~f:(fun map e ->
-      let name = Xml.attrib e "name"
-      and value = Xml.attrib e "value" |> Int64.of_string in
-      Enum_map.add map ~key:name ~data:value
-    ) ~init:map enums
-end
-
-let arg_parse () =
-  let specs = [
-    ("-major", Arg.Int (fun s -> major_version := s), "Major version of the OpenGL");
-    ("-minor", Arg.Int (fun s -> minor_version := s), "Minor version of the OpenGL");
-  ] in
-  let usega = "usega: enums <xml file> -major <major version> -minor <minor version>" in
-  Arg.parse specs (fun s -> xml_file := s) usega
+module Type_map = Map.Make(String)
+type t = {
+  name: string;
+  value: [`Int64 of int64 | `Int32 of int32]
+}
 
 let children = Util.children
 
-let extract_groups xml =
-  let groups = children xml "groups" in
-  let groups = List.map ~f:(Fn.flip children "group") groups |> List.join in
-  let extract e =
-    let name = Xml.attrib e "name" in
-    let enums = children e "enum" in
-    if List.length enums = 0 then None
-    else
-      let enums = List.map ~f:(fun x -> Xml.attrib x "name") enums in
-      Some (name, enums) in
-  List.map ~f:extract groups
-             |> List.fold ~f:(fun accum v ->
-               match v with
-               | Some v -> v :: accum 
-               | None -> accum
-             ) ~init:[]
-
-let extract_enums xml =
-  let enums = children xml "enums" in
-  let tbl = Enum_map.empty in 
-  List.fold_left ~f:(fun map e ->
-    children e "enum" |> Enum.merge map
-  ) ~init:tbl enums
-
-let construct_enums groups enums =
-  let convert_group_to_module_name name =
-    String.fold name ~init:[] ~f:(fun l c ->
-      if Char.is_uppercase c then Char.lowercase c :: '_' :: l else Char.lowercase c :: l
-    )
-             |> List.rev
-             |> String.of_char_list
-             |> fun s -> String.chop_prefix ~prefix:"_" s
-             |> Option.value_map ~default:s ~f:String.capitalize
-  in
-  let enum_to_let_decl enum =
-    let convert_const_to_let name = String.substr_replace_first name ~pattern:"GL" ~with_:"gl" in 
-    let matches = List.map enum ~f:(fun (key, data) ->
-      Printf.sprintf "let %s = %LdL" (convert_const_to_let key) data
-    ) in
-    String.concat ~sep:"\n" matches
-  in
-  let group_to_module (group, group_enums) =
-    let enum = List.filter group_enums ~f:(fun name -> Enum_map.existsi enums ~f:(fun ~key ~data -> key = name))
-             |> List.map ~f:(fun name -> (name, Enum_map.find_exn enums name))
-    in
-    let name = convert_group_to_module_name group in
-    let let_decls = enum_to_let_decl enum in
-    Printf.sprintf "module %s = struct\n%s\nend;;" name let_decls
-  in
-  List.map groups ~f:group_to_module |> String.concat ~sep:"\n"
-
-let () =
-  arg_parse ();
-  let xml = Xml.parse_file !xml_file in
-  try
-    let groups = extract_groups xml in
-    let enums = extract_enums xml in
-    construct_enums groups enums |> print_string
-
-  with Xml.Not_element s -> failwith (Printf.sprintf "%s do not have element is %s " !xml_file (Xml.to_string s))
+let parse xml =
+  let enums = children xml "enums"
+         |> List.map ~f:(Fn.flip children "enum")
+         |> List.join in 
+  let tbl = Type_map.empty in 
+  List.fold_left enums ~f:(fun map e ->
+    let name = Xml.attrib e "name"
+    and value = Xml.attrib e "value"
+    and typ = Xml.attribs e |> List.filter ~f:(fun (name, v) -> name = "type")
+        |> (function
+          | [] | [(_, "u")] -> `Int32
+          | [(_, "ull")] -> `Int64
+          | _ -> failwith "Unknown enum element format"
+        ) in 
+    let data = {
+      name;
+      value = match typ with
+      | `Int32 -> `Int32 Int32.(of_string value)
+      | `Int64 -> `Int64 Int64.(of_string value);
+    } in 
+    Type_map.add map ~key:name ~data
+  ) ~init:tbl
--- gen/gl_bindings/glreg_parser.ml
+++ gen/gl_bindings/glreg_parser.ml
@@ -6,23 +6,30 @@
    @author derui
    @since 0.2
 *)
-open Core.Std
+module List = Core.Std.List
+module Map = Core.Std.Map
+module String = Core.Std.String
+module Fn = Core.Std.Fn
+module Set = Core.Std.Set
+module Option = Core.Std.Option
 
 module Type_map = Map.Make(String)
+module Type_set = Set.Make(String)
 type t = {
   commands: Command.t Type_map.t;
   enums: Enum.t Type_map.t;
 }
 
-let filter_feature xml version =
+let filter_feature xml major minor =
+  let feature_version = float_of_string (Printf.sprintf "%d.%d" major minor) in
   let features = Util.children xml "feature" in
   List.filter features ~f:(fun feature ->
     let version' = Xml.attrib feature "number" |> float_of_string in
-    version >= version'
+    feature_version >= version'
   )
 
 (* extract commands in the feature and merge into a large commands *)
-let extract_feature map feature =
+let extract_commands map feature =
   let requirements = Util.children feature "require"
   and removings = Util.children feature "remove" in
   let required_commands = List.map ~f:(Fn.flip Util.children "command") requirements |> List.join in
@@ -30,29 +37,75 @@ let extract_feature map feature =
     let name = Xml.attrib command "name" in
     Type_map.add map ~key:name ~data:command
   ) ~init:map in 
+
   let removed_commands = List.map ~f:(Fn.flip Util.children "command") removings |> List.join in
   List.fold_left removed_commands ~f:(fun map command ->
     let name = Xml.attrib command "name" in
     Type_map.remove map name
   ) ~init:map
 
-let collect_required_commands xml major minor =
-  let feature_version = float_of_string (Printf.sprintf "%d.%d" major minor) in
-  let features = filter_feature xml feature_version in 
+let collect_required_commands features =
   let command_map = Type_map.empty in
   List.fold_left features ~f:(fun map feature ->
-    extract_feature map feature
+    extract_commands map feature
   ) ~init:command_map
 
 (* collect commands from <command> tag in xml. *)
-let collect_commands commands =
+let parse_commands xml commands =
   let command_map = Type_map.empty in
-  Type_map.fold commands ~f:(fun ~key ~data map ->
+  let command_elements = Util.child_exn xml "commands" |> Fn.flip Util.children "command" in 
+  let command_map = List.fold_left command_elements ~f:(fun map data ->
     let command' = Command.parse data in
-    Type_map.add map ~key ~data:command'
-  ) ~init:command_map
+    let _, name = command'.Command.proto in 
+    Type_map.add map ~key:name ~data:command'
+  ) ~init:command_map in
+
+  Type_map.fold commands ~f:(fun ~key ~data map ->
+    Type_map.add map ~key ~data:(Type_map.find_exn command_map key)
+  ) ~init:Type_map.empty
+
+let collect_required_enums features =
+(* extract commands in the feature and merge into a large commands *)
+  let extract_enums set feature =
+    let requirements = Util.children feature "require"
+    and removings = Util.children feature "remove" in
+    let required_enums = List.map ~f:(Fn.flip Util.children "enum") requirements |> List.join 
+    and removed_enums = List.map ~f:(Fn.flip Util.children "enum") removings |> List.join in
+
+    let set = List.fold_left required_enums ~f:(fun map enum ->
+      let name = Xml.attrib enum "name" in
+      Type_set.add map name
+    ) ~init:set in 
+
+    List.fold_left removed_enums ~f:(fun set enum ->
+      let name = Xml.attrib enum "name" in
+      Type_set.remove set name
+    ) ~init:set
+  in 
+  
+  let enum_set = Type_set.empty in
+
+  List.fold_left features ~f:(fun set feature ->
+    extract_enums set feature
+  ) ~init:enum_set |> Type_set.to_list
+
+(* Get enums flitered with feature requiring *)
+let filter_enums enum_map required =
+  let map = Type_map.empty in
+  List.fold_left required ~f:(fun map enum ->
+    let open Option.Monad_infix in
+    Type_map.find enum_map enum |>
+        Option.value_map ~f:(fun data -> Type_map.add map ~key:enum ~data) ~default:map
+  ) ~init:map
 
 let parse xml ~major ~minor = 
   try
-    collect_required_commands xml major minor |> collect_commands
+    let features = filter_feature xml major minor in
+    let commands = collect_required_commands features |> parse_commands xml in
+    let enums = Enum.parse xml in 
+    let enums = collect_required_enums features |> filter_enums enums in
+    {
+      commands;
+      enums;
+    }
   with _ -> failwith (Printf.sprintf "Error of parse %s" (Xml.to_string xml))
