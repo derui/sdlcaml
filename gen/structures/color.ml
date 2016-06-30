3eb07a64c25d208a00af74b0c1fcfae14e8e3edb
derui <derutakayu@gmail.com>
2016-06-30 23:08:47 +0900

    Add conversion function for Color module

--- gen/structures/color.ml
+++ gen/structures/color.ml
@@ -16,4 +16,13 @@ let a = field t "a" uint8_t
 let to_string {r;g;b;a} =
   Printf.sprintf "colors{r = %d;g = %d;b = %d;a = %d}" r g b a
 
+let of_ocaml c =
+  let str = make t in
+  let module U = Unsigned.UInt8 in
+  setf str r (U.of_int c.r);
+  setf str g (U.of_int c.g);
+  setf str b (U.of_int c.b);
+  setf str a (U.of_int c.a);
+  str
+
 let () = seal t
